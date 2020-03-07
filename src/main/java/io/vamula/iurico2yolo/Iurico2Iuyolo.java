97009a66eeb74445fa67cfab5d0ab421d6b32e5a
unknown <Joao Porto>
2020-03-07 16:05:17 -0300

    Pavingt the path to several files convertion

--- src/main/java/io/vamula/iurico2yolo/Iurico2Iuyolo.java
+++ src/main/java/io/vamula/iurico2yolo/Iurico2Iuyolo.java
@@ -2,22 +2,34 @@ package io.vamula.iurico2yolo;
 
 import java.io.IOException;
 
+import org.json.simple.parser.ParseException;
+
 public class Iurico2Iuyolo {
 	public static void main(String args[]) throws IOException {
-		if (args.length != 2) {
-			System.out.println("Usage: iurico2iuyolo <file-iurico-address> <file-iuyolo-address>");
+		if (args.length < 2 || args.length != 3 || !args[0].equals("-s")) {
+			System.out.println("For just one file conv: iurico2iuyolo <file-iurico-address> <file-iuyolo-address>");
+			System.out.println("For several files conv: iurico2iuyolo -s <folder-iurico-address> <folder-iuyolo-address>");
 			System.exit(0);
 		}
 		
 		try {
-			RicoFile ricoFile = new RicoFile(args[0]);
-			YoloFile yoloFile = new YoloFile(ricoFile);
-
-			yoloFile.writeFile(args[1]);
+			if (args.length == 3) {
+				
+			} else {
+				iurico2iuyolo(args[0], args[1]);
+			}
+			
 			System.out.println("Awesome! Everything is ready.");
 		} catch (Exception e) {
 			System.err.println("Conversion failed. Something seems be wrong:");
 			System.err.println(e.getMessage());
 		}
+	}
+	
+	public static void iurico2iuyolo(String addrRicoFile, String addrYoloFile) throws IOException, ParseException {
+		RicoFile ricoFile = new RicoFile(addrRicoFile);
+		YoloFile yoloFile = new YoloFile(ricoFile);
+
+		yoloFile.writeFile(addrYoloFile);
 	}
 }
